029fe55e8f76e27cf29c699465088c39248a50e5
Badr98-t <bt98.bt7@gmail.com>
2023-12-07 15:09:33 +0000

    bitra:rising-ify

--- rising_bitra.mk
+++ rising_bitra.mk
@@ -18,8 +18,30 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common bliss stuff.
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+# Inherit some common Rising stuff.
+$(call inherit-product, vendor/rising/config/rising.mk)
+
+# Device Identifiers
+RISING_MAINTAINER := Badr98-t
+RISING_CHIPSET := SDM870-5G
+RISING_STORAGE := 128gb
+RISING_RAM := 8gb
+RISING_DEVICE := bitra
+RISING_BATTERY := 5000mah
+RISING_DISPLAY := 2400×1080
+
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_HAS_UDFPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+WITH_GMS := true
+TARGET_ENABLE_BLUR := true
+TARGET_PREBUILT_PIXEL_LAUNCHER := false
+TARGET_FACE_UNLOCK_SUPPORTED := false
 
 # Enable extra UDFPS animations
 EXTRA_UDFPS_ANIMATIONS := true
@@ -34,7 +56,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := bliss_bitra
+PRODUCT_NAME := rising_bitra
 PRODUCT_DEVICE := bitra
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3370
