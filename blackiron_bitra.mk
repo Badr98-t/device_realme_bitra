604cbeb80070bc2210790b0a1b09c1fa2f6448f2
Badr98-t <bt98.bt7@gmsil.com>
2024-02-02 17:25:53 +0100

    bitra :fixes for udpfs

--- blackiron_bitra.mk
+++ blackiron_bitra.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/blackiron/config/common_full_phone.mk)
 # Blackiron
 BLACKIRON_BUILDTYPE := Official
-
+TARGET_HAS_UDFPS := true
 TARGET_BUILD_APERTURE_CAMERA := false
 TARGET_HAS_UDFPS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
